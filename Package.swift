c2c8e9777bc47d1d86e6b60fec79855e5b67c056
Diez B. Roggisch <deets@web.de>
2019-03-30 19:09:29 +0100

    Updated to latest swift tools

--- Package.swift
+++ Package.swift
@@ -1,5 +1,18 @@
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
-    name: "BinUtils"
+  name: "BinUtils",
+  products: [
+    .library(
+            name: "BinUtils",
+            targets: ["BinUtils"]),
+    ],
+    targets: [
+        .target(
+            name: "BinUtils",
+            dependencies: [],
+            path: "Sources"
+        ),
+    ]
 )
